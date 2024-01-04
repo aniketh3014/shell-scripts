9ea834dc650cf79e8bd54d6ac6c0a21d50cecf42
aniket <ghoshaniket1000@gmail.com>
2024-01-05 00:18:06 +0530

    Used if else conditionals

--- read.sh
+++ read.sh
@@ -3,5 +3,8 @@
 # read input from the user
 echo "what is you name?"
 read name
-echo "wow! you have a cool name $name"
-
+if [ $name ]; then
+	echo "That sound good to me $name"
+else
+	echo "Does't sound like anything to me!?"
+fi
