beeeb26fce66f895719d4a16eb37404eba9aaed5
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-02-24 19:21:36 +0530

    added comments in script and added awk command to get PID of a specific process

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -9,10 +9,14 @@
 # Version: 1
 ################################
 
-set -x # Debug mode
+set -x          # Debug mode
+set -e          # exits when there is any error while executing the script
+set -o pipefail # watches for pipe failures and exits the script
 
 df -h
 
 free -g
 
 nproc
+
+ps -ef | grep "amazon" | awk -F" " '{print $2}' # prints the specic detail wanted from the oput put of the ps -ef command
